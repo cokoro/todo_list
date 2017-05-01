099add99a9eab09a3ed5dafb92687cbbb4b37845
陈可心 <cokoro@foxmail.com>
2017-05-01 10:34:23 +0800

    add create & update method
    Change-Id: Id4efc22997eaa97cdb887985d0a624cff5001bce

--- src/todo_list/todo_item/serializer.py
+++ src/todo_list/todo_item/serializer.py
@@ -9,3 +9,14 @@ class TodoItemSerializer(serializers.Serializer):
     content = serializers.CharField(required=True, allow_blank=True, max_length=1024)
     check = serializers.BooleanField(required=False, default=False)
     deadline = serializers.DateTimeField(required=False)
+    
+    def create(self, validated_data):
+        return TodoItem.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.content = validated_data.get('content', instance.content)
+        instance.check = validated_data.get('check', instance.check)
+        instance.deadline = validated_data.get('deadline', instance.deadline)
+        instance.save()
+        return instance
